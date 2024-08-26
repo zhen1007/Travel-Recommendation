a084fd21be166ca0176400bb14e0cd409176b6b7
zhen1007 <zhen1007@outlook.com>
2024-08-26 00:03:28 -0400

    task 6

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,11 +1,20 @@
+const API_URL = "travel_recommendation_api.json";
+
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
 const clearBtn = document.getElementById("clearBtn");
 
 searchBtn.addEventListener("click", () => {
   const query = searchInput.value;
-  alert(`Searching for: ${query}`);
-  // Perform search logic here
+
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("fetched data", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 });
 
 clearBtn.addEventListener("click", () => {
