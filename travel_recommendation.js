87a973f43e0d405c1288c84d089d98e4996936cf
zhen1007 <zhen1007@outlook.com>
2024-08-26 01:12:03 -0400

    task 7,8

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,6 +4,41 @@ const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
 const clearBtn = document.getElementById("clearBtn");
 
+function showRecommended(data) {
+  const resultDiv = document.getElementById("result");
+  if (data.length == 0) {
+    resultDiv.innerHTML = "<p>No Recommendation Result</p>";
+  } else {
+    resultDiv.innerHTML = data
+      .map((item) => {
+        if ("cities" in item) {
+          return item.cities
+            .map((city) => {
+              return `<div class="card">
+                <img src="${city.imageUrl}" width="500px" height="300px" />
+                <div class="inner">
+                  <h3>${city.name}</h3>
+                  <p>${city.description}</p>
+                  <button>Visit</button>
+                </div>
+              </div>`;
+            })
+            .join();
+        } else {
+          return `<div class="card">
+            <img src="${item.imageUrl}" width="500px" height="300px" />
+            <div class="inner">
+              <h3>${item.name}</h3>
+              <p>${item.description}</p>
+              <button>Visit</button>
+            </div>
+          </div>`;
+        }
+      })
+      .join();
+  }
+}
+
 searchBtn.addEventListener("click", () => {
   const query = searchInput.value;
 
@@ -11,6 +46,18 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       console.log("fetched data", data);
+      const _query = query.toLowerCase();
+      const recommended = [];
+      if (["beach", "beaches"].includes(_query)) {
+        recommended.push(...data.beaches);
+      } else if (["temple", "temples"].includes(_query)) {
+        recommended.push(...data.temples);
+      } else if (["country", "countries"].includes(_query)) {
+        recommended.push(...data.countries);
+      } else {
+        console.log("No recommend data");
+      }
+      showRecommended(recommended);
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
